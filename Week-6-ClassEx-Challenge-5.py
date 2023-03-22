69d096cedaf1f8c8de480aed38ec0302b9ca4b9f
jinnychoe <125236103+jinnychoe@users.noreply.github.com>
2023-03-21 19:12:12 -0700

    Update Week-6-ClassEx-Challenge-5.py

--- Week-6-ClassEx-Challenge-5.py
+++ Week-6-ClassEx-Challenge-5.py
@@ -9,8 +9,8 @@ win = tk.Tk()  # creates window interface
 win.geometry("400x200")  # width x height in pixels
 win.title("Customer Information")  # label for the title
 
-lblLastname = tk.Label(win, text="enter the last name: ").grid(column=0, row=0)  # label widget
-lblFirstname = tk.Label(win, text="enter the first name: ").grid(column=0, row=1)
+lblLastname = tk.Label(win, text="Last name: ").grid(column=0, row=0)  # label widget
+lblFirstname = tk.Label(win, text="First name: ").grid(column=0, row=1)
 lblAddress = tk.Label(win, text="Address: ").grid(column=0, row=2)
 lblCity = tk.Label(win, text="City: ").grid(column=0, row=3)
 lblState = tk.Label(win, text="State: ").grid(column=0, row=4)
@@ -32,10 +32,10 @@ def submit():  # submit function
     messagebox.showinfo("Information", "Entered: " + LN.get() + "," + FN.get()  + "," + ADDRESS.get() + "," + CITY.get() + "," + STATE.get() + "," + ZIPCODE.get())  # display info
 
 LN = tk.StringVar()  # the StringVar manages the entry widget
-txtLastname = tk.Entry(win, width=12, textvariable=LN).grid(column=1, row=0)  # text entry widget
+txtLastname = tk.Entry(win, width=35, textvariable=LN).grid(column=1, row=0)  # text entry widget
 
 FN = tk.StringVar()
-txtFirstname = tk.Entry(win, width=12, textvariable=FN).grid(column=1, row=1)
+txtFirstname = tk.Entry(win, width=35, textvariable=FN).grid(column=1, row=1)
 
 ADDRESS = tk.StringVar()
 txtAddress = tk.Entry(win, width=35, textvariable=ADDRESS).grid(column=1, row=2)
@@ -50,12 +50,12 @@ ZIPCODE = tk.StringVar()
 tk.Entry(win, width=35, textvariable=ZIPCODE).grid(column=1, row=5)
 
 # command calls the click fxn
-btnSubmit = tk.Button(win, text="Submit", command=submit).grid(column=0, row=4)  # Button widget
+btnSubmit = tk.Button(win, text="Submit", command=submit).grid(column=0, row=6)  # Button widget
 
 # command calls quit fxn
-btnQuit = tk.Button(win, text="Quit", command=quit).grid(column=1, row=4)  # button widget
+btnQuit = tk.Button(win, text="Quit", command=quit).grid(column=1, row=6)  # button widget
 
 # call the function write
-btnWrite = tk.Button(win, text="Transfer", command=write).grid(column=1, row=4)  # button widget
+btnWrite = tk.Button(win, text="Transfer", command=write).grid(column=2, row=6)  # button widget
 
-win.mainloop()  # ensures ethe interfaces stays on the screen or window
+win.mainloop()  # ensures the interfaces stays on the screen or window
