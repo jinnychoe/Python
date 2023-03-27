e89b13b10fd27788c459eb8107cf0a21d4d8eab5
jinnychoe <125236103+jinnychoe@users.noreply.github.com>
2023-03-27 15:53:20 -0700

    Add files via upload

---
+++ Week-7-Class-Ex-Question-3.py
@@ -0,0 +1,30 @@
+#Jinny Choe
+#3/27/2023
+#Week 7 Class Exercise 3
+
+#This program demonstrates how to tokenize strings
+
+def main():
+    #strings to tokenize
+    str1 = 'one to three four'    
+    str2 = '10:20:30:40:50'
+    str3 = 'a/b/c/d/e/f'
+    
+    #display the tokens in each string
+    display_tokens(str1,' ')
+    print()
+    display_tokens(str2,':')
+    print()
+    display_tokens(str3,'/')
+
+#The display_tokens function displays the tokens in a string
+#the data parameter is the string to tokenize and the delimiter parameter is the delimiter
+
+def display_tokens(data, delimiter):
+    tokens = data.split(delimiter)
+    for item in tokens:
+        print(f'Token: {item}')
+
+#execute the main function
+if __name__ == '__main__':
+    main()
