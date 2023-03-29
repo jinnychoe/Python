6c3352af3592d434a362903da3bc2b621087049e
jinnychoe <125236103+jinnychoe@users.noreply.github.com>
2023-03-29 14:04:50 -0700

    Update Week-7-HW-String-1.py

--- Week-7-HW-String-1.py
+++ Week-7-HW-String-1.py
@@ -6,26 +6,24 @@
 def str_input():
     while True:
         user_input = input("Type in one word: ") #user input for a word
-        if user_input.isalpha():
+        if user_input.isalpha(): #checks if input is only alphabetical characters
             return user_input
         else: #if there are any non alpha characters ask for input again
             print("Type in only alpha characters!")
         
-def count_vowels(string):
-    count = 0
-    for a in string:
+def count_vowels(string): #counts number of vowels
+    count = 0 #intializes vowel counter
+    for a in string: #checks each character in string to see if it is a vowel
         if a.lower() in "aeiou":
-            count += 1
-        #count number of vowels
-    return count
+            count += 1 #if character is vowel, adds to vowel counter
+    return count #returns count of vowels
 
-def count_cons(string):
-    count = 0
-    for a in string:
-        if a.lower() not in "aeiou":
-            count += 1 
-        #count number of consonants
-    return count
+def count_cons(string): #count number of consonants
+    count = 0 #initializes consonant counter
+    for a in string: #checks each character in string to see if it is consonant
+        if a.lower() not in "aeiou": #of character is consonant, adds to consonant counter
+            count += 1    
+    return count #returns count of consonant
 
 def main():
     an_input = str_input()
